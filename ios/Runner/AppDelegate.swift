8063aee0ca829a753d9e8c4ec091b7a6d4675dec
Max-dev-dev <codemagicmax@gmail.com>
2025-07-02 12:26:55 +0300

    ewrtgy

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,53 @@
 import Flutter
 import UIKit
+import AppTrackingTransparency
+import AdSupport
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+  private let IDENTIFIER_CHANNEL = "app.id.values"
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+      GeneratedPluginRegistrant.register(with: self)
+
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+
+    FlutterMethodChannel(
+      name: IDENTIFIER_CHANNEL,
+      binaryMessenger: controller.binaryMessenger
+    ).setMethodCallHandler { call, result in
+      switch call.method {
+     case "requestTrackingAndGetIdfa":
+       if #available(iOS 14, *) {
+         ATTrackingManager.requestTrackingAuthorization { status in
+           DispatchQueue.main.async {
+             let idfa = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+             result(idfa)
+           }
+         }
+       } else {
+         let idfa = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+         result(idfa)
+       }
+
+      case "getIdfv":
+        let idfv = UIDevice.current.identifierForVendor?.uuidString
+        result(idfv)  // returns nil only if something is truly wrong
+
+      default:
+        result(FlutterMethodNotImplemented)
+      }
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+    override func applicationDidBecomeActive(_ application: UIApplication) {
+      if #available(iOS 14, *) {
+        ATTrackingManager.requestTrackingAuthorization { _ in }
+      }
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:ios_f_n_mathsplashcountbignumbers_3199/app.dart';
+import 'package:ios_f_n_mathsplashcountbignumbers_3199/ver_screen.dart';
+
+class AppConstants {
+  static const String oneSignalAppId = "ba40d8ae-fbe2-4572-b04b-12124a0ac394";
+  static const String appsFlyerDevKey = "EsJBZj76R5fCiere38Z6Dd";
+  static const String appID = "6748059722";
+
+  static const String baseDomain = "splendiferous-grandiose-championship.space";
+  static const String verificationParam = "CBG4EwkJ";
+
+  static const String splashImagePath = 'assets/images/splash_background.png';
+}
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  final now = DateTime.now();
+  final dateOff = DateTime(2024, 7, 2, 20, 00);
+
+  final initialRoute = now.isBefore(dateOff) ? '/white' : '/verify';
+  runApp(RootApp(
+    initialRoute: initialRoute,
+    whiteScreen: MainApp(),
+  ));
 }
